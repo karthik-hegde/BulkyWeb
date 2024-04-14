e7172f0d45e4ef1a53b870dcb2477fd9205990b7
karthik-hegde <karthikhegde77@gmail.com>
2024-04-14 11:11:11 +0530

    added additional pages. auth routing

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,66 +1,131 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.ViewModels;
 using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnv;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnv)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnv = webHostEnv;
         }
         public IActionResult Index()
         {
-            List<Product> ProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> ProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
+
             return View(ProductList);
         }
 
-        public IActionResult Create() {
-            return View();
+        public IActionResult Upsert(int? id)
+        {
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString(),
+            });
+            ProductVM productVM = new()
+            {
+                CategoryList = CategoryList,
+                Product = new Product()
+            };
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+
         }
 
-        [HttpPost]
-        public IActionResult Create(Product product)
+        [HttpPost, ActionName("Upsert")]
+        public IActionResult UpsertPOST(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(product);
+                string assetPath = _webHostEnv.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(assetPath, @"images\product");
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // delete old image
+                        var oldImagePath = Path.Combine(assetPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.CreateNew))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
             }
-            return View(product);
+            else
+            {
+                IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+                productVM.CategoryList = CategoryList;
+            }
+            return View(productVM);
         }
 
-        public IActionResult Edit(int id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            Product product = _unitOfWork.Product.Get(u => u.Id == id);
-            if (product != null)
-            {
-                return View(product);
-            }
-            return NotFound();
-        }
+        //public IActionResult Edit(int id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product product = _unitOfWork.Product.Get(u => u.Id == id);
+        //    if (product != null)
+        //    {
+        //        return View(product);
+        //    }
+        //    return NotFound();
+        //}
 
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                    _unitOfWork.Product.Update(product);
-                    _unitOfWork.Save();
-                    TempData["success"] = "Product Updated successfully";
-                    return RedirectToAction("Index");
+        //[HttpPost]
+        //public IActionResult Edit(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //            _unitOfWork.Product.Update(product);
+        //            _unitOfWork.Save();
+        //            TempData["success"] = "Product Updated successfully";
+        //            return RedirectToAction("Index");
 
-            }
-            return View(product);
-        }
+        //    }
+        //    return View(product);
+        //}
         public IActionResult Delete(int id)
         {
             if (id == null)
@@ -76,7 +141,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
         }
 
         [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(Product product) {
+        public IActionResult DeletePOST(Product product)
+        {
             if (ModelState.IsValid)
             {
                 Product? obj = _unitOfWork.Product.Get(u => u.Id == product.Id);
@@ -91,6 +157,18 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
             return View("Index");
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> ProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+
+            return Json(new { data = ProductList });
+        } 
+
+        #endregion
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,11 +32,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
